abfd436d4810eb1c968694fd0a80402a3d341e3b
ampatishan <aampatishan@gmail.com>
2024-05-08 16:04:28 -0400

    env setup done

---
+++ environment.sh
@@ -0,0 +1,8 @@
+module load python/3.10.2
+module load scipy-stack/2023b
+module load StdEnv/2023  gcc/12.3
+module load opencv/4.8.1
+module load arrow
+#virtualenv venv
+source venv/bin/activate
+pip install -r environment.txt
